86dbb1f2e6de6d8cb2996a9a6572fc0838e31960
Proxy539 <roman.filiyutich@gmail.com>
2026-06-22 11:46:25 +0300

    Chapter 2

---
+++ src/chapter2/NyPizza.java
@@ -0,0 +1,40 @@
+package chapter2;
+
+import java.util.Objects;
+
+public class NyPizza extends Pizza {
+
+    public enum Size {SMALL, MEDIUM, LARGE};
+
+    private final Size size;
+
+    public static class Builder extends Pizza.Builder<Builder> {
+        private final Size size;
+
+        public Builder(Size size) {
+            this.size = Objects.requireNonNull(size);
+        }
+
+        @Override
+        NyPizza build() {
+            return new NyPizza(this);
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+    }
+
+    private NyPizza(Builder builder) {
+        super(builder);
+        size = builder.size;
+    }
+
+    static void main() {
+        NyPizza pizza = new NyPizza.Builder(Size.SMALL)
+                .addTopping(Topping.SAUSAGE)
+                .addTopping(Topping.ONION)
+                .build();
+    }
+}
